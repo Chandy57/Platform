b931a27f295f8f5220c85717849b300bd77df581
Chandy <chandeluredude57@gmail.com>
2024-03-21 19:41:10 -0300

    basic movement
    simple movement and floor implemented -- albeit movement is broken for the moment

---
+++ PlatformGame/scripts/character_control.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+@export var speed := 10.0
+var direction : Vector2
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if(Input.is_action_pressed("left")):
+		direction.x = -1
+	if(Input.is_action_pressed("right")):
+		direction.x = 1
+	
+	apply_force(direction * speed)
